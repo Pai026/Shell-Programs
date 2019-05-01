76acfb23f2b62cc8b3b6f6fb86100632b12c16dd
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:36:55 +0530

    Update exp2.sh

--- exp2.sh
+++ exp2.sh
@@ -3,13 +3,13 @@ read -p "enter a number" a;
 t=0
 r=0
 n=$a
-while [ $a -gt 0 ]
+while [ $a -gt 0 ] #-gt represents greater than
 do 
-	t=$(( $a % 10 ))
+	t=$(( $a % 10 )) #$(( )) is used to store the value to the variable or the value of $a % 10 would be stored as string
 	r=$(( $r * 10 + $t ))
 	a=$(( $a / 10 ))
 done
-if [ $r -eq $n ]
+if [ $r -eq $n ] #-eq represents equal to
 then 
 	echo "The number is palindrome"
 else
