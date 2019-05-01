e5f31394145c32ceeb04755c8320863673fe0ac7
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:41:41 +0530

    Update exp7.sh

--- exp7.sh
+++ exp7.sh
@@ -1,8 +1,8 @@
 #Program to traverse through the files and folders of the current directory
 
-for i in *
+for i in * # * stores all the files and folders to i
 do
-	if [ -f $i ]
+	if [ -f $i ] # checks whether i is file or not if file then prints its file or prints its a folder
 	then
 		echo "$i  file"
 	else
