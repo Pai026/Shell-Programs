cc4b19d4bf3c786c4eb5b692768c54dbf0909e62
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:54:07 +0530

    Update exp11.sh

--- exp11.sh
+++ exp11.sh
@@ -4,3 +4,7 @@ for i in *
 do
 	wc -c $i | awk '{s+=$1}END{print s}'
 done
+#wc -c returns the number of characters present in the file
+#athinte output awk kodutu
+#so first column contains the nos of characters present in each file athinte sum edukan we do s+=$1
+#Then END ethummbo we print s
