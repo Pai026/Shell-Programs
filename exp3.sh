1b9c412d5d9b92876aad1987b3afb8c4ad2ba344
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:38:09 +0530

    Update exp3.sh

--- exp3.sh
+++ exp3.sh
@@ -1,8 +1,8 @@
 #program to check the largest number from the given set
-large=$1
-for i in "$@"
+large=$1 #stores value of first argument to large
+for i in "$@" #$@ stores the value of command line arguments to i
 do
-	if [ $large -lt  $i  ]
+	if [ $large -lt  $i  ] #-lt represents less than
 	then	
 		large=$i 
 	fi
