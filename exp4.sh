3155089525ad053bef9b7aeab8d26ac237f0a911
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:39:24 +0530

    Update exp4.sh

--- exp4.sh
+++ exp4.sh
@@ -1,8 +1,8 @@
 #program to find word entered by user in a file
 read -p "Enter the word to be searched" a;
-if [ -f $1 ]
+if [ -f $1 ] #-f checks whether the filename passed as 1st argument is present or not
 then 
-	if grep  "$a" $1
+	if grep  "$a" $1 #grep pattern is used to search whether the argument is present in file or not
 	then 
 		echo "exist"
 	else
