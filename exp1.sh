5de63e37e763f6ea5fa12b21bf91695e71e82592
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:35:28 +0530

    Update exp1.sh

--- exp1.sh
+++ exp1.sh
@@ -1,8 +1,8 @@
 #program to find factorial of a number
 pro=1
-read -p "Enter the value" a
+read -p "Enter the value" a #read from the user
 i=1
-while [ $i -le $a ]
+while [ $i -le $a ] #-le represent less than or equal to
 do
 	pro=$(( $pro * $i ))
 	i=$(( $i + 1 ))
