d391f617879dd8fcf5ed43edeaa58125efb872e0
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 21:44:48 +0530

    Update exp1.sh

--- exp1.sh
+++ exp1.sh
@@ -1,5 +1,5 @@
 #program to find factorial of a number
-pro=1
+pro=1 #pro is first assigned 1 just like we do in c program for finding product
 read -p "Enter the value" a #read from the user
 i=1
 while [ $i -le $a ] #-le represent less than or equal to
