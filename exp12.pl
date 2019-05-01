e9c2168383bed8d5a33e7639b3c44f70bf81b3af
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:57:06 +0530

    Update exp12.pl

--- exp12.pl
+++ exp12.pl
@@ -1,7 +1,7 @@
 #perl program to input string and find whether it contains int/char
 print "Enter anything";
-$a=<>;
-if ( $a =~ /[a-z,A-Z]/)
+$a=<>;#ithu works like scanf takes value and stores in $a
+if ( $a =~ /[a-z,A-Z]/) #comapares whether $a is in between a-z and A-Z
 {	print "Its a character";
 }
 else
