7baac130975766b7d0358773e036ab9ff82c8a8f
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 21:36:42 +0530

    Add files via upload

--- exp11.sh
+++ exp11.sh
@@ -1,10 +1,9 @@
 #Program to find the total number of characters in all files in the directory
-sum=0
 for i in *
 do
-	wc -c $i | awk '{s+=$1}END{print s}'
+	if [ -f $i ]
+	then
+		wc -c $i > abc.txt
+	fi
 done
-#wc -c returns the number of characters present in the file
-#athinte output awk kodutu
-#so first column contains the nos of characters present in each file athinte sum edukan we do s+=$1
-#Then END ethummbo we print s
+awk '{s+=$1}END{print s}' abc.txt
