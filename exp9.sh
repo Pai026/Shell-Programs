32139c05b3ae7818203cd71ce9a5e5a2f9166527
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:50:48 +0530

    Update exp9.sh

--- exp9.sh
+++ exp9.sh
@@ -1,2 +1,4 @@
 #Program to print ram usage using shell script
 ps aux|awk '{print $3}'
+#ps aux returns list of processes currently running
+#3rd column contains ram status so that column is printed
