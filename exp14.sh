381551af7d642a5747e7015ce3c96547ebcc7281
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 21:24:03 +0530

    Update exp14.sh

--- exp14.sh
+++ exp14.sh
@@ -3,7 +3,7 @@ awk '{s+=$2}END{print "average for 1 subject is" s/3}' student.txt
 awk '{k+=$3}END{print "average for 2 subject is" k/3}' student.txt
 awk '{m+=$4}END{print "average for 3 subject is" m/3}' student.txt
 awk '{
-if($4>=50 || $2>=50 || $3>=50)
+if($4>=50 || $2>=50 || $3>=50)#|| represents or
 {a+=1
 }
 }
