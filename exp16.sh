1a6ad1a894aad56d5237d7fcf6e4af96a1e3de0d
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 21:04:18 +0530

    Update exp16.sh

--- exp16.sh
+++ exp16.sh
@@ -1,2 +1,5 @@
 #Program to traverse through the subdirectories of the current directory using awk
 ls -l -d */ |awk '{print $9}'
+# ls -l command use cheyumbo namak files inte privilleges kanam athu ingane ayirikum(-rw-rwx-rw) so ithil mumbil
+# d nu parnju indel athinte artham its a folder appo a d indo nu nokan annu -d */ use cheytekune
+# and $9 represents the column which contains the name of all files and folder in the directory
