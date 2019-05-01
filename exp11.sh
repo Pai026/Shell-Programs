418f30532fb3808ce9ac70c0f7f1955e131afc97
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 21:38:59 +0530

    Update exp11.sh

--- exp11.sh
+++ exp11.sh
@@ -3,7 +3,8 @@ for i in *
 do
 	if [ -f $i ]
 	then
-		wc -c $i > abc.txt
+		wc -c $i > abc.txt #stores the no of characters of each file in abc.txt
 	fi
 done
 awk '{s+=$1}END{print s}' abc.txt
+#takes the first column in abc.txt which contains the number of characters then adds to get the total number of characters
