25edeb85a9b624b6d56b64c5ebf60adc584c4d10
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-01 20:52:11 +0530

    Update exp10.sh

--- exp10.sh
+++ exp10.sh
@@ -1,4 +1,5 @@
 #program to find the free memory available or %hard disk available
 echo "Available %hard disk is "
 ps aux | awk '{k+=$4}END{print 100-k}'
-
+#same as sum of size here the sum of hard disk usage is stored in k 
+#and athu 100 ninu minus cheyta free ayitula hard disk value kittum in percentage
