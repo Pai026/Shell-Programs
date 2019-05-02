2bc45721f41ceb2064fd238f806d63a555db1bb8
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-02 20:45:19 +0530

    Update exp17.sh

--- exp17.sh
+++ exp17.sh
@@ -1,10 +1,10 @@
 #program to check whether the given number is amstrong or not 
-a=$1
+a=$1 #number is passed as command line argument
 n=$a
 while [ $a -gt 0 ]
 do
 	d=$(( $a % 10 ))
-	r=$(( $r + $d * $d * $d ))
+	r=$(( $r + $d * $d * $d )) # 153 = 1^3+5^3+3^3
 	a=$(( $a / 10 ))
 done
 if [ $r -eq $n ]
