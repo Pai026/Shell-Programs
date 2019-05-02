b5a0da65cf785235971b77a808b33a7d9f20f9a0
Abhiram Pai <abhirampai1999@gmail.com>
2019-05-02 21:01:44 +0530

    Update exp16.sh

--- exp16.sh
+++ exp16.sh
@@ -3,3 +3,5 @@ ls -l -d */ |awk '{print $9}'
 # ls -l command use cheyumbo namak files inte privilleges kanam athu ingane ayirikum(-rw-rwx-rw) so ithil mumbil
 # d nu parnju indel athinte artham its a folder appo a d indo nu nokan annu -d */ use cheytekune
 # and $9 represents the column which contains the name of all files and folder in the directory
+# ee program awk use chumbo exp 15 il if use cheyta kandu pidichekane thats the only difference between
+# exp16 and exp15
